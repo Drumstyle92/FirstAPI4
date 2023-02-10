855f6b9329a5f7d1587d310e86eea94f390e2b6d
Dino <dino15031992@gmail.com>
2023-02-10 22:43:59 +0100

    add commit

--- src/main/java/co/develhope/FirstAPI4/controller/HeaderController.java
+++ src/main/java/co/develhope/FirstAPI4/controller/HeaderController.java
@@ -1,24 +1,24 @@
 package co.develhope.FirstAPI4.controller;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.http.HttpHeaders;
+
+import java.net.InetSocketAddress;
 
 /**
  * @author Drumstyle92
  * Spring Boot controller class.
  */
 @RestController
+
 public class HeaderController {
-    /**
-     * @param headers parameter with annotation with which we can get a response on HTTP information.
-     * @return returns a reply about the host name and the port name.
-     * Get method which takes and retrieves a response in HTTP headers.
-     */
     @GetMapping("/headers")
-    public String getHeaders(@RequestHeader("host") String headers){
-        return headers;
+    public String getHeader(@RequestHeader HttpHeaders headers) {
+        InetSocketAddress host = headers.getHost();
+        return "Host: " + host.getHostName() + ", Port: " + host.getPort();
     }
 }
